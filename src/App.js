73778a42c984948386e632101ac81b82a4f8009e
Luiz Fernando <devfernandoof@gmail.com>
2020-09-05 00:20:33 -0300

    🚧 global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import GlobalStyle from "./styles/global";
+
 function App() {
-  return <div className="App"></div>;
+  return <GlobalStyle />;
 }
 
 export default App;
