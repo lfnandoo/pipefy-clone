fe674597140f777e6a42820bdc7496cadae1fce3
Luiz Fernando <devfernandoof@gmail.com>
2020-09-05 23:05:38 -0300

    🌟 final commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 
+import BoardContext from "../Board/context";
+
 import { useDrag, useDrop } from "react-dnd";
 
 import { Container, Label } from "./styles";
 
-export default function Card({ data, index }) {
+export default function Card({ data, index, listIndex }) {
   const ref = React.useRef();
+  const { move } = React.useContext(BoardContext);
 
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: "CARD", index },
+    item: { type: "CARD", index, listIndex },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
@@ -17,15 +20,37 @@ export default function Card({ data, index }) {
   const [, dropRef] = useDrop({
     accept: "CARD",
     hover(item, monitor) {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
       const draggedIndex = item.index;
       const targetIndex = index;
 
-      if (draggedIndex === targetIndex) {
+      if (
+        draggedIndex === targetIndex &&
+        draggedListIndex === targetListIndex
+      ) {
         return;
       }
 
       const targetSize = ref.current.getBoundingClientRect();
-      console.log(targetSize);
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return;
+      }
+
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
     },
   });
 
